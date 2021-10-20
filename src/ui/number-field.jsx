f8c96245ac93741512aae9cbf18c507fe89d20f9
Mark Tellez (Devmentor.live) <mark@devmentor.live>
2021-10-20 12:42:11 -0500

    added masked phone

--- src/ui/number-field.jsx
+++ src/ui/number-field.jsx
@@ -1,7 +1,7 @@
 import TextField from "./text-field";
 
 export function numbersOnly(val) {
-  return val.toString().replace(/[^0-9]/, "");
+  return val.toString().replace(/[^0-9]/g, "");
 }
 
 export default function NumberField({
--- src/ui/phone-field.jsx
+++ src/ui/phone-field.jsx
@@ -6,11 +6,24 @@ export default function PhoneField({
   value,
   onChange = () => {},
 }) {
+  function mask(val) {
+    var reg = /^(\d{1,3})(?:\s*(\d{1,3})(?:\s*(\d{1,4})(?:\s*(\d{1,4}))?)?)?$/;
+
+    return val.replace(
+      reg,
+      (_, code, prefix, suffix, extension) =>
+        (code.length > 0 ? `(${code}` : "") +
+        (prefix ? `) ${prefix}` : "") +
+        (suffix ? ` - ${suffix}` : "") +
+        (extension ? ` ${extension}` : "")
+    );
+  }
+
   return (
     <NumberField
       placeholder={placeholder}
       label={"Phone number"}
-      value={value}
+      value={mask(value)}
       maxLength={10}
       onChange={(val) => onChange(val)}
       renderLeftIcon={() => <PhoneIcon className="w-4 h-4" />}
